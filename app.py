76ee58e17ecc97f04e26a1707dde36c05b6cac3e
Mikołaj Hojda <32071801+mikolajhojda@users.noreply.github.com>
2025-04-12 15:57:09 +0200

    Flask app with Docker

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1.0/predict', methods=['GET'])
+def predict():
+    x = float(request.args.get('x', 0))
+    y = float(request.args.get('y', 0))
+
+    sum = x + y
+    prediction = 1 if sum > 5.8 else 0
+
+    return jsonify({
+        "prediction": prediction,
+        "features": {"x": x, "y": y}
+    })
+
+if __name__ == '__main__':
+    app.run()
